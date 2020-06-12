ec98e22a083706321f3eb45c2a3c05d5b06b1f34
Keith <ongkeeaik@gmail.com>
2020-06-12 21:32:29 +0800

    add boothstrap modal and form

--- src/component/sidebar/SideBar.js
+++ src/component/sidebar/SideBar.js
@@ -1,15 +1,24 @@
 import React from "react";
+import AddEvent from "../modal/AddEvent";
 
 const SideBar = () => {
   return (
     <div className="col-lg-3">
-      <button className="btn btn-primary btn-block">Create New Event</button>
+      <button
+        data-toggle="modal"
+        data-target="#add-event"
+        className="btn btn-primary btn-block"
+      >
+        Create New Event
+      </button>
       <div className="m-t-20">
         <br />
         <div className="external-event bg-primary">Watch Movie</div>
         <div className="external-event bg-success">Call Friend</div>
         <div className="external-event bg-danger">Eat Dinner</div>
       </div>
+
+      <AddEvent />
     </div>
   );
 };
