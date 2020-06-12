05e0e795d85731a2515f30d125173b41ce9e2693
Keith <ongkeeaik@gmail.com>
2020-06-12 22:38:26 +0800

    add event function

--- src/component/modal/AddEvent.js
+++ src/component/modal/AddEvent.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import EventForm from "./EventForm";
 
 const AddEvent = () => {
-  const inputChange = (e) => {};
-  const onCheckBoxChange = () => {};
-  const handleChange = () => {};
-  const onInputChange = (input) => {};
+  const [color, setColor] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [checkbox, setCheckBox] = useState(false);
+  const [showtime, setShowTime] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const colors = ["Primary", "Success", "Info", "Warning", "Danger"];
+  const colorObj = {
+    primary: "#0275d8",
+    success: "#5cb85c",
+    info: "#5bc0de",
+    warning: "#f0ad4e",
+    danger: "#d9534f",
+  };
+
+  const inputChange = (e) => {
+    setEventName(e.target.value);
+  };
+
+  const onCheckBoxChange = (e) => {
+    if (e.target.checked === true) {
+      setShowTime(true);
+      setCheckBox(true);
+    } else {
+      setShowTime(false);
+      setCheckBox(false);
+    }
+  };
+
+  const onInputChange = (propertyName) => (e) => {
+    if (propertyName === "startdate") {
+      setStartDate(e);
+    }
+    if (propertyName === "enddate") {
+      setEndDate(e);
+    }
+  };
+  const handleChange = (e) => {
+    if (e.target.value !== "Select Color") {
+      setColor(e.target.value);
+    } else {
+      setColor("");
+    }
+  };
   const createEvent = () => {};
   const closeModal = () => {};
   return (
@@ -14,18 +55,19 @@ const AddEvent = () => {
         modalId="add-event"
         title="Add Event"
         closeModal={closeModal}
-        eventname="Watch movies"
+        eventname={eventName}
         inputChange={inputChange}
-        checkbox={false}
+        checkbox={checkbox}
         onCheckBoxChange={onCheckBoxChange}
-        showtime={false}
-        startDate={new Date()}
-        endDate={new Date()}
+        showtime={showtime}
+        startDate={startDate}
+        endDate={endDate}
         onInputChange={onInputChange}
-        color="Primary"
-        colors={["Primary", "Danger"]}
+        color={color}
+        colors={colors}
         handleChange={handleChange}
         eventType={createEvent}
+        colorObj={colorObj}
         buttonText="Save"
       />
     </div>
