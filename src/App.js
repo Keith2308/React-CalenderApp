cb36aec4bcca090c09b6fdb9b186752b572ae004
Keith <ongkeeaik@gmail.com>
2020-06-12 16:58:02 +0800

    add calender react component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 import "./App.css";
 import Main from "./component/Main";
 
