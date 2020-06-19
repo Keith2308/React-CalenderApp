b32d09d32ee5237ba853235d7577ea3de1a6f88a
Keith <ongkeeaik@gmail.com>
2020-06-19 16:09:18 +0800

    toast component style

--- src/component/toast/Toast.js
+++ src/component/toast/Toast.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Toast.css";
 
 const Toast = () => {
   return (
     <>
       <div className="notification-container notification-bottom-right">
-        <div className="notification toast">
+        <div className="notification toast" style={{ backgroundColor: "red" }}>
           <button>X</button>
           <p className="notification-title">Notification</p>
           <p className="notification-subtitle">Overdue 5 mins ago</p>
